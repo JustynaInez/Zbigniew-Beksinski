da9467a5a8acc599626e014065c5a4bb96ce750d
Justyna Ciarachowicz <j.ciarachowicz@gmail.com>
2020-06-26 22:23:19 +0200

    addedFunction

--- js/script.js
+++ js/script.js
@@ -22,45 +22,61 @@ default:
     console.log("Szaro i zimno na dworze");
 }
 
-let buttonChangeBackgroundColor= document.querySelector(".buttonChangeBackgroundColor");
-let container = document.querySelector(".container");
-let changeColor = document.querySelector (".changeColor");
 
-buttonChangeBackgroundColor.addEventListener("click", () => {
-    changeColor.classList.toggle("container");
-});
+const buttonChangeBackgroundColor= document.querySelector(".buttonChangeBackgroundColor");
+const container = document.querySelector(".container");
+const changeColor = document.querySelector (".changeColor");
+
+let changeBackgroundColor = () =>
+ {
+     changeColor.classList.toggle("container"); 
+};
+
+buttonChangeBackgroundColor.addEventListener("click", changeBackgroundColor);
+
 
 let article = document.querySelector(".article");
 let pink = document.querySelector(".pink");
 let button = document.querySelector(".button");
 
-button.addEventListener("click", () => {
+let changeParagrapf = () => {
     if (article.classList.contains("form")) {
         pink.classList.toggle("article");
     } else {
         article.remove();
     }
-});
+};
 
-button.addEventListener("click", () => {
+button.addEventListener("click", changeParagrapf);
+
+let changeTextofButton = () => {
     if (pink.classList.contains("article")) {
         button.innerText = "Niespodzianka! :)";
     }
     else {
         button.innerText = "I jeszcze raz! :D";
     }
-});
+};
+
+button.addEventListener("click", changeTextofButton); 
+
 
 let obrazek = document.querySelector(".image1");
 let button2 = document.querySelector(".change");
 let nowy = document.querySelector(".nowy");
 
-
-button2.addEventListener("click", () => {
+let changeImage = () => {
     if (obrazek.classList.toggle("nowy")) {
         obrazek.src = "https://pl-static.z-dn.net/files/d77/5f87f01852ee279a1aada65dfd1532af.jpg";
     } else {
         obrazek.src = "https://fanbojizycie.files.wordpress.com/2015/02/beksinski1.jpg";
     }
-});
+};
 
+button2.addEventListener("click", changeImage);
+
+
+function count (number) {
+    return number * 3;
+}
+console.log (count(10));
