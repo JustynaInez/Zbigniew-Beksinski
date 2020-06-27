fb0f88690d6a4cad705a13057580e3611213c5cd
Justyna Ciarachowicz <j.ciarachowicz@gmail.com>
2020-06-27 19:31:16 +0200

    repair js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,49 @@
 console.log("Elo");
 
 {
-    const buttonChangeBackgroundColor = document.querySelector(".buttonChangeBackgroundColor");
-    const changeColor = document.querySelector(".changeColor");
 
-    let changeBackgroundColor = () => {
-        changeColor.classList.toggle("changeColor");
-    };
-
-    let article = document.querySelector(".article");
-    let pink = document.querySelector(".pink");
-    let button = document.querySelector(".button");
-
-    let changeTextofButton = () => {
-        if (pink.classList.contains("article")) {
-            button.innerText = "Niespodzianka! :)";
-        }
-        else {
-            button.innerText = "I jeszcze raz! :D";
-        }
-        (article.classList.contains("form"))
-        pink.classList.toggle("article");
-    };
-
-    let obrazek = document.querySelector(".image1");
-    let button2 = document.querySelector(".change");
-
-    let changeImage = () => {
-        if (obrazek.classList.toggle("nowy")) {
-            obrazek.src = "https://pl-static.z-dn.net/files/d77/5f87f01852ee279a1aada65dfd1532af.jpg";
-        } else {
-            obrazek.src = "https://fanbojizycie.files.wordpress.com/2015/02/beksinski1.jpg";
-        }
+    const buttonChangeBackgroundColor= document.querySelector(".buttonChangeBackgroundColor");
+    const changeColor = document.querySelector (".changeColor");
+    
+    let changeBackgroundColor = () =>
+     {
+         changeColor.classList.toggle("container"); 
     };
     
     buttonChangeBackgroundColor.addEventListener("click", changeBackgroundColor);
 
-    button.addEventListener("click", changeTextofButton);
 
-    button2.addEventListener("click", changeImage);
+let article = document.querySelector(".article");
+let pink = document.querySelector(".pink");
+let button = document.querySelector(".button");
+
+let changeTextofButton = () => {
+    if (pink.classList.contains("article")) {
+        button.innerText = "Niespodzianka! :)";
+    }
+    else {
+        button.innerText = "I jeszcze raz! :D";
+    }
+    (article.classList.contains("form")) 
+        pink.classList.toggle("article");
+};
+
+button.addEventListener("click", changeTextofButton); 
+
+
+let obrazek = document.querySelector(".image1");
+let button2 = document.querySelector(".change");
+
+
+let changeImage = () => {
+    if (obrazek.classList.toggle("nowy")) {
+        obrazek.src = "https://pl-static.z-dn.net/files/d77/5f87f01852ee279a1aada65dfd1532af.jpg";
+    } else {
+        obrazek.src = "https://fanbojizycie.files.wordpress.com/2015/02/beksinski1.jpg";
+    }
+};
+
+button2.addEventListener("click", changeImage);
 
 }
+
